4b088eecb93989a94cb3a287115b2acc74f12c3f
jinwoo1210 <james7377@naver.com>
2024-03-14 13:10:40 +0900

    feat : 게임씬, 게임메니저 수정

--- Assets/Scripts/Scene/GameScene.cs
+++ Assets/Scripts/Scene/GameScene.cs
@@ -2,44 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameScene : Singleton<GameScene>
+public class GameScene : BaseScene
 {
     public static GameScene instance;
 
-    [Header("# Player Inpo")]
-    public int hp;
-    public int maxHp = 100;
-    public int level;
-    public int kill;
-    public int exp;
-    public int[] nextExp = { 3, 5, 10, 70, 130, 190, 290, 420, 600, 800 };
-    [Header("# Game Object")]
-    public PoolManager pool;
-    public Player player;
-    public LevelUp uiLvevlUp;
-
-    protected override void Awake()
+    public override IEnumerator LoadingRoutine()
     {
-        instance = this;
+        yield return null;
     }
 
-    private void Start()
+    public override void Clear()
     {
-        hp = maxHp;
-
-        // 임시 스크립트 (첫번째 캐릭터 선택)
-        uiLvevlUp.Select(0);
-    }
-
-    public void GetExp()
-    {
-        exp++;
-
-        if (exp == nextExp[Mathf.Min(level, nextExp.Length - 1)])
-        {
-            level++;
-            exp = 0;
-            uiLvevlUp.show();
-        }
+        
     }
 }
